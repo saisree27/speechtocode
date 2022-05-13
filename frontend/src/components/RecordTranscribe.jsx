399d7053e1848989935ea2d1d83c5c0a50be4946
saisree27 <saigautambonam@gmail.com>
2022-05-13 19:31:23 -0400

    Add functioning recording

--- frontend/src/components/RecordTranscribe.jsx
+++ frontend/src/components/RecordTranscribe.jsx
@@ -1,12 +1,23 @@
 import Recorder from "react-mp3-recorder";
+import React, { useState } from "react";
 
 export default function RecordTranscribe() {
+  var [recorded, setRecorded] = useState(false);
+  var [recording, setRecording] = useState(null);
+
   let _onComplete = (blob) => {
     console.log("Recording", blob);
+    setRecorded(true);
+    setRecording(blob);
+
+    const player = new Audio(URL.createObjectURL(blob));
+    player.play();
   };
 
   let _onError = (error) => {
     console.log("Error", error);
+    setRecorded(false);
+    setRecording(null);
   };
 
   return (

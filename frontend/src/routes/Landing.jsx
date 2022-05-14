f97bbee74bbdb11c1375104a4bf33c2ea3323b82
kinshukphalke <phalkekinshuk@gmail.com>
2022-05-15 00:42:24 +0400

    Minor UI Changes

--- frontend/src/routes/Landing.jsx
+++ frontend/src/routes/Landing.jsx
@@ -1,4 +1,5 @@
 import "../css/landing.css";
+import "../css/main.css";
 import { Link } from "react-router-dom";
 import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
@@ -30,9 +31,9 @@ export default function Landing() {
       </div>
 
         <div className="topnav">
-          <a className="active" href="/">Home</a>
-          <a className="active" href="/learn">Speech2Code</a>
-          <a className="active" href="/tutorial">Tutorial</a>
+          <a className="active fw-light font-monospace" href="/">HOME</a>
+          <a className="active fw-light font-monospace" href="/learn">SPEECH2CODE</a>
+          <a className="active fw-light font-monospace" href="/tutorial">TUTORIAL</a>
         </div>
         
         <div>
@@ -46,32 +47,36 @@ export default function Landing() {
           </Link>
         </div>
         <center>
-        <h1 className='title'>Welcome to Speech-To-Code!</h1>
-        <br />
-        <Link to="/learn"><button id = 'startButton'> Use the Tool</button></Link>
-        <br />
-        <br />
-        <Link to="/tutorial"><button id = 'tutorialButton'>Follow Tutorial</button></Link>
+        <p className='position-relative fs-1 text-white fw-light font-monospace title'>Welcome to Speech-To-Code!</p>
         </center>
-        <br />
-        <br />
-        <br />
 
-        <h2 className='title'>Purpose</h2>
-        <div><center><p  className='abouttext'>This website is intended to help beginner coders learn the fundemental syntax 
-        of programming languages. It translated English phrases spoken by the user into functional code. The user has the option 
+        {/*<h2 className='title'>Purpose</h2>*/}
+        <div><center><p className='position-relative text-justify text-white fw-light font-monospace abouttext'>This website is intended to help beginner coders learn the fundemental syntax
+        of programming languages. It translated English phrases spoken by the user into functional code. The user has the option
         to translate their English phrases into Java, Python, or JavaScript.</p></center></div>
+        <center className="mt-2">
+        <Link to="/learn"><button id = 'startButton' className="me-5 btn btn-info fw-light font-monospace"> Use the Tool</button></Link>
+        <Link to="/tutorial"><button id = 'tutorialButton' className="ms-2 btn btn-secondary fw-light font-monospace">Follow Tutorial</button></Link>
+        </center>
+        {/*<div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
+        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>*/}
+        {/*<h2 className='title'>Creators</h2>
+        <div><center><p  className='abouttext'>This project was created by Saigautam Bonam, Nicholas Cai, Kinshuk Phalke, and Dhruv Sharma.</p></center></div>*/}
+        <footer className="footer">
+            <div className="text-center">
+              <p className="fw-light font-monospace">
+                <strong>Speech2Code</strong> by Saigautam Bonam, Nicholas Cai, Kinshuk Phalke, and Dhruv Sharma.
+               </p>
+            </div>
+          </footer>
+        {/*<div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
-        <h2 className='title'>Creators</h2>
-        <div><center><p  className='abouttext'>This project was created by Saigautam Bonam, Nicholas Cai, Kinshuk Phalke, and Dhruv Sharma.</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
-        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
-        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
-        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
-        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
+        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>*/}
+
 
       </div>
   );

f5a8cdd1b3fbb386fe5bf23119fe4acb08e5a508
ShDhruvSh <67289288+ShDhruvSh@users.noreply.github.com>
2022-05-14 03:32:16 -0400

    added text

--- frontend/src/routes/Landing.jsx
+++ frontend/src/routes/Landing.jsx
@@ -45,12 +45,16 @@ export default function Landing() {
         <br />
         <Link to="/tutorial"><button id = 'tutorialButton'>Follow Tutorial</button></Link>
         </center>
-        
+        <br />
+
+        <h2 className='title'>Purpose</h2>
+        <div><center><p  className='abouttext'>This website is intended to help beginner coders learn the fundemental syntax 
+        of programming languages. It translated English phrases spoken by the user into functional code. The user has the option 
+        to translate their English phrases into</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
-        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
-        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
-        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
+        <h2 className='title'>Creators</h2>
+        <div><center><p  className='abouttext'>This project was created by Saigautam Bonam, Nicholas Cai, Kinshuk Phalke, and Dhruv Sharma.</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>

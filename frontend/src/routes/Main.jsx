29af4fbb92a1b4e42e267a3ba12d13a49266b634
saisree27 <saigautambonam@gmail.com>
2022-05-14 03:17:35 -0400

    Fix css duplicate

--- frontend/src/routes/Main.jsx
+++ frontend/src/routes/Main.jsx
@@ -26,7 +26,105 @@ export default function Main() {
     selectOnLineNumbers: true,
   };
 
-  const customSelect = {};
+  const colors = {
+    /*
+     * multiValue(remove)/color:hover
+     */
+    danger: "purple",
+
+    /*
+     * multiValue(remove)/backgroundColor(focused)
+     * multiValue(remove)/backgroundColor:hover
+     */
+    dangerLight: "grey",
+
+    /*
+     * control/backgroundColor
+     * menu/backgroundColor
+     * option/color(selected)
+     */
+    neutral0: "grey",
+
+    /*
+     * control/backgroundColor(disabled)
+     */
+    neutral5: "orange",
+
+    /*
+     * control/borderColor(disabled)
+     * multiValue/backgroundColor
+     * indicators(separator)/backgroundColor(disabled)
+     */
+    neutral10: "pink",
+
+    /*
+     * control/borderColor
+     * option/color(disabled)
+     * indicators/color
+     * indicators(separator)/backgroundColor
+     * indicators(loading)/color
+     */
+    neutral20: "white",
+
+    /*
+     * control/borderColor(focused)
+     * control/borderColor:hover
+     */
+    // this should be the white, that's normally selected
+    neutral30: "grey",
+
+    /*
+     * menu(notice)/color
+     * singleValue/color(disabled)
+     * indicators/color:hover
+     */
+    neutral40: "green",
+
+    /*
+     * placeholder/color
+     */
+    // seen in placeholder text
+    neutral50: "white",
+
+    /*
+     * indicators/color(focused)
+     * indicators(loading)/color(focused)
+     */
+    neutral60: "purple",
+    neutral70: "purple",
+
+    /*
+     * input/color
+     * multiValue(label)/color
+     * singleValue/color
+     * indicators/color(focused)
+     * indicators/color:hover(focused)
+     */
+    neutral80: "white",
+
+    // no idea
+    neutral90: "pink",
+
+    /*
+     * control/boxShadow(focused)
+     * control/borderColor(focused)
+     * control/borderColor:hover(focused)
+     * option/backgroundColor(selected)
+     * option/backgroundColor:active(selected)
+     */
+    primary: "white",
+
+    /*
+     * option/backgroundColor(focused)
+     */
+    primary25: "purple",
+
+    /*
+     * option/backgroundColor:active
+     */
+    primary50: "brown",
+    primary75: "grey",
+  };
 
   useEffect(() => {
     document.title = "This is a title";
@@ -69,7 +167,12 @@ export default function Main() {
           value={language}
           onChange={setLanguage}
           options={languageOptions}
-          styles={customSelect}
+          theme={(theme) => ({
+            ...theme,
+            colors: {
+              ...colors,
+            },
+          })}
         />
       </div>
 

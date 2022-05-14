00ed0d0b3e872fb6333de3a1016bb86d90d19af6
ShDhruvSh <67289288+ShDhruvSh@users.noreply.github.com>
2022-05-13 23:33:56 -0400

    stuff

--- frontend/src/routes/Landing.jsx
+++ frontend/src/routes/Landing.jsx
@@ -1,11 +1,16 @@
 import "../css/landing.css";
 import { Link } from "react-router-dom";
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
 
 /**
  * Landing page of application
  * Associated with route "/"
  */
 export default function Landing() {
+  useEffect(() => {
+    document.title = "This is a title"
+  }, [])
   return (
     <div style={{
       backgroundImage: "url(/bgimg.jpeg)",
@@ -15,20 +20,26 @@ export default function Landing() {
       width: '100vw',
       height: '100vh'
       }}>
-      <div>
-          <Link to="/" id='tohome'>
-          <h4 id='icontext'>Speech2Code</h4>
-          <img src='mic.png'></img>
-        </Link>
+
+
+        <div>
+
+
+
+
+            <Link to="/" id='tohome'>
+            <h4 id='icontext'>Speech2Code</h4>
+            <img src='mic.png'></img>
+          </Link>
+        </div>
+        <center>
+        <h1 className='title'>Welcome to Speech-To-Code!</h1>
+        <Link to="/learn"><button id = 'startButton'> Use the Tool</button></Link>
+        <br />
+        <br />
+        <Link to="/tutorial"><button id = 'startButton'>Follow Tutorial</button></Link>
+        </center>
+        <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
       </div>
-      <center>
-      <h1 className='title'>Welcome to Speech-To-Code!</h1>
-      <Link to="/learn"><button id = 'startButton'> Use the Tool</button></Link>
-      <br />
-      <br />
-      <Link to="/tutorial"><button id = 'startButton'>Follow Tutorial</button></Link>
-      </center>
-      <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
-    </div>
   );
 }

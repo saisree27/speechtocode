332c56f1e4f333bacb0786f8235e31b28d9e1e59
ShDhruvSh <67289288+ShDhruvSh@users.noreply.github.com>
2022-05-14 01:26:17 -0400

    pushing installed monaco editor stuff
    breh

--- frontend/src/routes/Landing.jsx
+++ frontend/src/routes/Landing.jsx
@@ -42,6 +42,7 @@ export default function Landing() {
         <Link to="/tutorial"><button id = 'startButton'>Follow Tutorial</button></Link>
         </center>
         <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
+
       </div>
   );
 }

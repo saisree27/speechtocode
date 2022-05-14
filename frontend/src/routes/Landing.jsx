fcf5a42a48fe3ac51f835037a4968d7ad3e4c50d
ShDhruvSh <67289288+ShDhruvSh@users.noreply.github.com>
2022-05-13 22:48:03 -0400

    VERY impactful changes :/
    breh

--- frontend/src/routes/Landing.jsx
+++ frontend/src/routes/Landing.jsx
@@ -7,14 +7,28 @@ import { Link } from "react-router-dom";
  */
 export default function Landing() {
   return (
-    <div>
+    <div style={{
+      backgroundImage: "url(/bgimg.jpeg)",
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      width: '100vw',
+      height: '100vh'
+      }}>
+      <div>
+          <Link to="/" id='tohome'>
+          <h4 id='icontext'>Speech2Code</h4>
+          <img src='mic.png'></img>
+        </Link>
+      </div>
       <center>
-      <h1>Landing Page</h1>
-      <Link to="/learn">Use the Tool</Link>
+      <h1 className='title'>Welcome to Speech-To-Code!</h1>
+      <Link to="/learn"><button> Use the Tool</button></Link>
       <br />
       <br />
-      <Link to="/tutorial">Follow Tutorial</Link>
+      <Link to="/tutorial"><button>Follow Tutorial</button></Link>
       </center>
+      <div><center><p  className='abouttext'>*insert stuff and developed by here*</p></center></div>
     </div>
   );
 }
